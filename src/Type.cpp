ecea1d0c375c678639cac12d336b15bea322e2dd
Brett Menzies <scimor5@gmail.com>
2016-11-24 13:40:49 -0800

    Array types retain their length

--- src/Type.cpp
+++ src/Type.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-const Type Type::INT("int", false, false);
-const Type Type::BOOL("bool", false, false);
-const Type Type::VOID("void", false, false);
-const Type Type::CHAR("char", false, false);
-const Type Type::NONE("undefined", false, false);
+const Type Type::INT("int", NON_ARRAY, false);
+const Type Type::BOOL("bool", NON_ARRAY, false);
+const Type Type::VOID("void", NON_ARRAY, false);
+const Type Type::CHAR("char", NON_ARRAY, false);
+const Type Type::NONE("undefined", NON_ARRAY, false);
 
 ostream& operator<<(ostream& os, const Type& t){
     os << t.toString();
